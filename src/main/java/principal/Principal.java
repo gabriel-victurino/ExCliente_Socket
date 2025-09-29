1afbf0564cbeca38301b53036ae3af1752ee9f2b
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-09-29 10:01:29 -0300

    Exercício Cliente Socket

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,24 @@
+package principal;
+
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        try {
+            Socket servidor = new Socket("10.146.193.118", 4444);//endereco e porta
+            PrintWriter out = new PrintWriter(
+                    servidor.getOutputStream(), true);
+            String mensagem = "";
+            while (!mensagem.equals("fim")) {
+                mensagem = JOptionPane.
+                        showInputDialog("Digite a Mensagem ou fim para sair");
+                out.println(mensagem); //escreve a mensagem
+            }
+        } catch (IOException io) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
